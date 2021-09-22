5c0a45726b488fe02c5a5c6d82004f9d66fde10c
veera_bugide <90595885+veerabugide@users.noreply.github.com>
2021-09-22 11:47:56 +0530

    Add files via upload

---
+++ Day 1.sql
@@ -0,0 +1,44 @@
+-- creation of database;
+
+
+create database student_info;
+
+-- using database;
+
+
+use student_info;
+
+
+-- creting table ;
+
+create table sale(salesId int not null,salesName varchar(20),address varchar(20));
+
+insert into sale values(1,'sahoo','Banglore');
+
+
+show tables;
+
+
+select * from sale;               -- select statement;
+
+
+select salesId from sale;         -- select statment with specifi column;
+
+
+select salesId from sale where salesName='sahoo';  -- where condition
+;
+
+select * from sale order by salesId;
+select o.id,o.productName,s.salesId from sale s, order_sales o
+
+
+where s.salesId=o.sales_Id;  -- alias
+
+
+
+select count(*) from sale;  -- aggregation functions
+
+
+
+select * from sale group by salesName;  -- group by
+
---
+++ Day 2.sql
@@ -0,0 +1,70 @@
+alter table sale add column city varchar(20); -- alter table  ;
+
+
+
+update sale set city='Banglore'
+ where sales_id =1;    -- update table;
+
+
+
+drop table sale;  -- delete table ;
+
+ 
+
+drop database assignment;  -- delete database;
+
+
+
+truncate table sale;    -- truncate table;
+
+
+
+delete from sale
+ where sales_Id=1;     -- deleting the row;
+
+ 
+
+select o.id,o.productName,s.salesId
+ 
+from sale s, order_sales o
+
+where s.salesId=o.sales_Id;       -- alias
+
+
+
+create table order_sales
+(
+id int primary key,
+
+productName varchar(15),
+
+address varchar(15),
+
+salesId int,
+
+constraint FK_order foreign key(salesId) references Sale(salesId));   --  using primary key, foreign key and not null;
+
+
+
+select o.id,o.productName,s.salesId 
+from sale s 
+
+inner join order_sales o
+ 
+on s.salesId=o.salesId;    -- Inner join;
+
+
+
+select o.id,o.productName,s.salesId 
+from sale s
+ 
+left join order_sales o 
+
+on s.salesId=o.salesId;  -- left join;
+
+select o.id,o.productName,s.salesId 
+from sale s
+ 
+left join order_sales o 
+
+on s.salesId=o.salesId;    -- right join;
